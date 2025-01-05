a12c663ef460eb708a4c8f27acb49799102599c5
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2025-01-05 03:26:26 +0200

    add: utils

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * from 'https://raw.githubusercontent.com/MAKS11060/deno-libs/refs/heads/main/debug/mod.ts'
+
---
+++ utils.ts
@@ -0,0 +1,47 @@
+/**
+ * @example
+ * serveTcp({key, cert, port: 80}, async (conn) => {
+ *   console.log(conn.remoteAddr)
+ *   conn.close()
+ * })
+ */
+export const serveTcp = async (
+  options: Deno.TcpListenOptions,
+  handler: (conn: Deno.TcpConn) => Promise<void>
+) => {
+  const listener = Deno.listen(options)
+  for await (const conn of listener) {
+    try {
+      handler(conn).catch((e) => {
+        console.error(e)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
+/**
+ * @example
+ * const key = Deno.readTextFileSync(Deno.env.get('KEY')!)
+ * const cert = Deno.readTextFileSync(Deno.env.get('CERT')!)
+ * serveTlsTcp({key, cert, port: 443}, async (conn) => {
+ *   console.log(conn.remoteAddr)
+ *   conn.close()
+ * })
+ */
+export const serveTlsTcp = async (
+  options: Deno.ListenTlsOptions & Deno.TlsCertifiedKeyPem,
+  handler: (conn: Deno.TlsConn) => Promise<void>
+) => {
+  const listener = Deno.listenTls(options)
+  for await (const conn of listener) {
+    try {
+      handler(conn).catch((e) => {
+        console.error(e)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
